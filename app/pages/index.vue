f51bff3ca6a874e1994a988de4d603bc781e36c7
Axel <axelmychro@gmail.com>
2025-12-26 21:41:52 +0700

    posts reorder

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
   const tags = {
-    dev: {
-      label: 'Dev',
-      color: 'primary',
-      variant: 'outline'
-    },
     other: {
       label: 'other',
       color: 'secondary',
       variant: 'subtle'
+    },
+    dev: {
+      label: 'Dev',
+      color: 'primary',
+      variant: 'outline'
     }
   } as const
 
   const posts = ref([
+    {
+      badge: tags.other,
+      image: '/posts/a_social_repose/image.png',
+      date: '2025-12-26',
+      title: 'a social repose',
+      description: 'how should this "good" cs student spend his free time?',
+      to: '/posts/a-social-repose'
+    },
     {
       badge: tags.other,
       image: '/posts/hello_world/image.png',
@@ -20,14 +28,6 @@
       title: 'hello world!',
       description: 'this is the first post, everything a work in progress~',
       to: '/posts/hello-world'
-    },
-    {
-      badge: tags.other,
-      image: '/posts/a_social_repose/image.png',
-      date: '2025-12-25',
-      title: 'a social repose',
-      description: 'how should this "good" cs student spend his free time?',
-      to: '/posts/a-social-repose'
     }
   ])
 </script>
