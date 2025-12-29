add4ec0ac1b681d515fbc4aa247c8a8e992dc2b2
Axel <axelmychro@gmail.com>
2025-12-29 17:55:02 +0700

    chore: retyped content variables

--- content.config.ts
+++ content.config.ts
@@ -1,18 +1,17 @@
 import { defineContentConfig, defineCollection } from '@nuxt/content'
-import { z } from 'zod'
 
 export default defineContentConfig({
   collections: {
     posts: defineCollection({
       type: 'page',
       source: 'posts/**/index.md',
-      schema: z.object({
-        badge: z.string(),
-        date: z.string(),
-        image: z.string(),
-        title: z.string(),
-        description: z.string()
-      })
+      schema: {
+        badge: 'string',
+        date: 'string',
+        image: 'string',
+        title: 'string',
+        description: 'string'
+      }
     })
   }
 })
